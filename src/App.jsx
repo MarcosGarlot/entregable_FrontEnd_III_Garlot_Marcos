53cba5ca1df41119b1114f199f5c2493dd107f12
MarcosGarlot <garlotmarcos@gmail.com>
2023-05-29 22:29:59 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card/>
+    </div>
+    </>
+  )
+}
+
+export default App
